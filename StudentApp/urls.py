a3b14606fdbae95c5c5fd135cfcf6b8f395119d5
punitlanjewar <pblanjewar22@gmail.com>
2023-11-06 02:17:06 +0530

    files Update

--- StudentApp/urls.py
+++ StudentApp/urls.py
@@ -3,6 +3,9 @@ from StudentApp import views
 
 
 urlpatterns = [
-    path('', views.login_fun, name='login'),
-    path('reg', views.register_fun, name='reg'),
+    path('', views.login_fun, name='login'), # redirect to login page
+    path('reg', views.register_fun, name='reg'), # redirect to register page
+    path('home', views.home_fun, name='home'), # redirect to home page
+    path('add_course', views.addcourse_fun, name='add_course'), # redirect to course add page
+    
 ]
--- StudentApp/views.py
+++ StudentApp/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
+from StudentApp.models import Course
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def login_fun(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        user_name = request.POST['txtName']
+        user_password = request.POST['txtPassword']
+        l1 = authenticate(username=user_name, password = user_password)
+        if l1 is not None:
+            if l1.is_superuser: # for checking superuser or not
+                request.session['Uname'] = user_name
+                return redirect('home')
+        else:      
+            return render(request, 'login.html', {'Msg': 'Username And Password is Incorrect'})
+    else:
+        return render(request, 'login.html')    
 
 
 def register_fun(request):
-    return render(request, 'register.html')
+    if request.method == 'POST': # this block for submit button
+        user_name = request.POST['txtName']
+        user_password = request.POST['txtPassword']
+        user_email = request.POST['txtEmail']
+        r1 = User.objects.create_superuser(username = user_name, password = user_password, email = user_email)
+        r1.save()
+        return redirect('login')
+    else: # this block for hyperlink
+        return render(request, 'register.html')
+    
+
+def home_fun(request):
+    return render(request, 'home.html', {'Name': request.session['Uname']})
+
+
+def addcourse_fun(request):
+    if request.method == 'POST': 
+        c1 = Course()
+        c1.course_name = request.POST['txtCourseName']
+        c1.course_duation = request.POST['txtCourseDuration']
+        c1.course_fees = int(request.POST['txtCourseFees'])
+        c1.save()
+        return render(request, 'addcourse.html', {'Msg': 'Course Successfully Added'})
+    else: # this block for hyperlink
+        return render(request, 'addcourse.html')
+
